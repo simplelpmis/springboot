05bbf8bb305af228fa5acc09c127d925f4709c45
simba <simplelpmis@hotmail.com>
2018-08-14 11:21:02 +0800

    slim jar

---
+++ README.md
@@ -0,0 +1,2 @@
+* 瘦身之后启动命令：
+java -Dloader.path=./lib -jar springboot.jar
--- src/main/java/com/simple/web/admin/job/HelloJob.java
+++ src/main/java/com/simple/web/admin/job/HelloJob.java
@@ -1,6 +1,8 @@
 package com.simple.web.admin.job;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,11 +15,17 @@ import java.util.Date;
  */
 @Component
 public class HelloJob {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloJob.class);
+
+
     @Value("${hello.world}")
     private String hello;
 
     @Scheduled(cron = "${hello.cron}")
-    public void sayHello() {
-        System.out.println(hello + ":" + new Date());
+    public void sayHello() throws InterruptedException {
+        logger.info(hello + "1:" + new Date());
+        Thread.sleep(6000);
+        logger.info(hello + "2:" + new Date());
     }
 }
